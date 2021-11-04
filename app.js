adfcc208cdfc38b898eff8cf1be3e8d1e7a8788d
Hugo Gabriel <hugogabrieltjj@gmail.com>
2021-11-03 22:37:27 -0300

    commit for github

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const fetchPokemon = () => {
-  const url = `https://pokeapi.co/api/v2/pokemon/`
+  const getPokemonUrl = id = `https://pokeapi.co/api/v2/pokemon/${id}`
 
-  fetch(url)
-    .then(response => response.json())
-}
+  const pokemonPromisses = []
 
-fetchPokemon()
+  for (let i = 1; i <= 150; i++) {
+    pokemonPromisses.push(fetch(getPokemonUrl(i)).then(response => response.json()))
+  }
+    Promisse.all(pokemonPromisses).then(pokemons => {
+      console.log(pokemons)
+    })
+  }
+
+fetchPokemon() 
